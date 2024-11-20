3f4e286a7087a5dfdac08df0dccbe0e7ad7bc4f5
Alexander <alexanderreuter713@gmail.com>
2024-11-20 10:43:50 +0100

    dasdsa

--- ordination-test/obj/Debug/net6.0/ordination-test.AssemblyInfo.cs
+++ ordination-test/obj/Debug/net6.0/ordination-test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ordination-test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73a6264fd56bc999c8269c72052a5336519e4139")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00d3dfb40f3026bcac79210659a77e12af89f385")]
 [assembly: System.Reflection.AssemblyProductAttribute("ordination-test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ordination-test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
